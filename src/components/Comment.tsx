acf09ae0202e74e847ff059e3438d44a8eb5548f
Wellington Albino <well.renato@hotmail.com>
2022-07-08 13:42:05 -0300

    primeiro commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,53 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react';
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps){
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/wellrenato12.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Wellington Renato</strong>
+                            <time title="03 de Junho às 17:17h" dateTime="2022-06-03 17:17:30">Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
